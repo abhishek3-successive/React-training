d39956804aa294e1359b11479e8f231cb4630b46
Abhishek Gupta <abhishek.gupta1@abhishekguptavsr-35.successivetech.com>
2025-06-25 15:29:26 +0530

    feat : add new pages

--- src/app/Assignment2/page.jsx
+++ src/app/Assignment2/page.jsx
@@ -103,7 +103,7 @@ export default function HomePage() {
             fontWeight: "bold",
             transition: "transform 0.3s, box-shadow 0.3s",
           }}>
-          <Link href="/Assignment2/Question7">Question-6</Link>
+          <Link href="/Assignment2/Question7">Question-7</Link>
         </li>
           <li style={{
             background: "black",
@@ -114,7 +114,7 @@ export default function HomePage() {
             fontWeight: "bold",
             transition: "transform 0.3s, box-shadow 0.3s",
           }}>
-          <Link href="/Assignment2/Question8">Question-6</Link>
+          <Link href="/Assignment2/Question8">Question-8</Link>
         </li>
           <li style={{
             background: "black",
@@ -125,7 +125,84 @@ export default function HomePage() {
             fontWeight: "bold",
             transition: "transform 0.3s, box-shadow 0.3s",
           }}>
-          <Link href="/Assignment2/Question9">Question-6</Link>
+          <Link href="/Assignment2/Question9">Question-9</Link>
+        </li>
+         <li style={{
+            background: "black",
+            padding: "20px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+            textAlign: "center",
+            fontWeight: "bold",
+            transition: "transform 0.3s, box-shadow 0.3s",
+          }}>
+          <Link href="/Assignment2/Question10">Question-10</Link>
+        </li>
+         <li style={{
+            background: "black",
+            padding: "20px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+            textAlign: "center",
+            fontWeight: "bold",
+            transition: "transform 0.3s, box-shadow 0.3s",
+          }}>
+          <Link href="/Assignment2/Question11">Question-11</Link>
+        </li>
+         <li style={{
+            background: "black",
+            padding: "20px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+            textAlign: "center",
+            fontWeight: "bold",
+            transition: "transform 0.3s, box-shadow 0.3s",
+          }}>
+          <Link href="/Assignment2/Question12">Question-12</Link>
+        </li>
+         <li style={{
+            background: "black",
+            padding: "20px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+            textAlign: "center",
+            fontWeight: "bold",
+            transition: "transform 0.3s, box-shadow 0.3s",
+          }}>
+          <Link href="/Assignment2/Question13">Question-13</Link>
+        </li>
+         <li style={{
+            background: "black",
+            padding: "20px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+            textAlign: "center",
+            fontWeight: "bold",
+            transition: "transform 0.3s, box-shadow 0.3s",
+          }}>
+          <Link href="/Assignment2/Question14">Question-14</Link>
+        </li>
+         <li style={{
+            background: "black",
+            padding: "20px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+            textAlign: "center",
+            fontWeight: "bold",
+            transition: "transform 0.3s, box-shadow 0.3s",
+          }}>
+          <Link href="/Assignment2/Question15">Question-15</Link>
+        </li>
+         <li style={{
+            background: "black",
+            padding: "20px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+            textAlign: "center",
+            fontWeight: "bold",
+            transition: "transform 0.3s, box-shadow 0.3s",
+          }}>
+          <Link href="/Assignment2/Question16">Question-16</Link>
         </li>
       </ul>
     </main>
