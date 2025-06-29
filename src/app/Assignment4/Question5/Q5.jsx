bb32d3e4af2fa5004a8be5be36fa2bb6bc292590
Abhishek Gupta <abhishek.gupta1@abhishekguptavsr-35.successivetech.com>
2025-06-29 15:42:53 +0530

    feat : add new questions

---
+++ src/app/Assignment4/Question5/Q5.jsx
@@ -0,0 +1,28 @@
+// 5.Construct a select dropdown menu with multiple options. 
+// Control the selected option using state. When an option is selected, display a message showing the selected value.
+'use client'
+import { useState } from "react";
+
+export default function Dropdown(){
+    const [select , setselect] = useState()
+
+    const handleselect = (e)=> setselect(e.target.value); 
+    const selectData = [
+        'option1',
+        'option2',
+        'option3'
+    ]
+    return(
+        <>
+        <select value={select} onChange={handleselect}>
+                {selectData.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+
+            <h1> your selected option is : {select}</h1>
+        </>
+    );
+}
